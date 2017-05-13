7e8b0f7bf8a733e72f57fa86fb18f9d801213263
Martin Unzner <martin.u@posteo.de>
2017-05-13 19:21:41 +0200

    Add initial implementation

---
+++ engines.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const services = [
+    {
+        name: 'pons',
+        baseUrl: 'http://de.pons.com/%C3%BCbersetzung?l=',
+        translationUrls: {
+            'Deutsch' : {
+                'English' : 'deen',
+                'Français' : 'defr',
+                'Italiano' : 'deit',
+                'Español (Si!)' : 'dees'
+            }
+        },
+        lookupUrl : '&q='
+    },
+    {
+        name: 'leo',
+        baseUrl: 'http://dict.leo.org/',
+        translationUrls: {
+            'Deutsch' : {
+                'English' : 'englisch-deutsch',
+                'Français' : 'französisch-deutsch',
+                'Italiano' : 'italienisch-deutsch',
+                'Español (Si!)' : 'spanisch-deutsch'
+            }
+        },
+        lookupUrl : '/'
+    }
+];
+
+function appendIFrames() {
+    services.forEach(function(service) {
+        $('#iframes').append('<iframe id="'+service.name+'Frame" src="about:blank" width="600" height="400"><p>Whoopsie! This prototype requires IFRAMEs to work. Sorry!</p></iframe>');
+    });
+}
+
+function assembleUrls() {
+    services.forEach(function(service) {
+        const srcLng = $('#srcLng').val();
+        const dstLng = $('#dstLng').val();
+        const translationUrl = service.translationUrls[srcLng][dstLng];
+        const word = encodeURIComponent($('#word').val());
+        const targetUrl = service.baseUrl + translationUrl + service.lookupUrl + word;
+        console.log("Loading " + targetUrl + " for service " + service.name);
+        $('#'+service.name+'Frame').attr('src', targetUrl);
+    });
+}
+
+function loadFrames() {
+    if (!$('#ponsFrame').length) {
+        appendIFrames();
+    }
+    assembleUrls();
+}
