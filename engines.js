740821cbfce3858fd9a1ddc6d7fa82fc275e9786
Martin Unzner <martin.u@posteo.de>
2017-05-13 23:19:17 +0200

    Apply autoformat

--- engines.js
+++ engines.js
@@ -1,43 +1,42 @@
 'use strict';
 
-const services = [
-    {
-        name: 'pons',
-        generateLookupUrl: function(srcLng, dstLng, word) {
-            const translationUrls = {
-                'Deutsch' : {
-                    'English' : 'deen',
-                    'Français' : 'defr',
-                    'Italiano' : 'deit',
-                    'Español (Si!)' : 'dees'
-                }
-            };
-            return '//de.pons.com/%C3%BCbersetzung?l=' +
-                translationUrls[srcLng][dstLng] +
-                '&q=' + word;
-        }
-    },
-    {
-        name: 'leo',
-        generateLookupUrl: function(srcLng, dstLng, word) {
-            const translationUrls = {
-                'Deutsch' : {
-                    'English' : 'englisch-deutsch',
-                    'Français' : 'französisch-deutsch',
-                    'Italiano' : 'italienisch-deutsch',
-                    'Español (Si!)' : 'spanisch-deutsch'
-                }
-            };
-            return '//dict.leo.org/' +
-                translationUrls[srcLng][dstLng] +
-                '/' + word;
-        }
+const services = [{
+    name: 'pons',
+    generateLookupUrl: function(srcLng, dstLng, word) {
+        const translationUrls = {
+            'Deutsch': {
+                'English': 'deen',
+                'Français': 'defr',
+                'Italiano': 'deit',
+                'Español (Si!)': 'dees'
+            }
+        };
+        return '//de.pons.com/%C3%BCbersetzung?l=' +
+            translationUrls[srcLng][dstLng] +
+            '&q=' + word;
     }
+},
+{
+    name: 'leo',
+    generateLookupUrl: function(srcLng, dstLng, word) {
+        const translationUrls = {
+            'Deutsch': {
+                'English': 'englisch-deutsch',
+                'Français': 'französisch-deutsch',
+                'Italiano': 'italienisch-deutsch',
+                'Español (Si!)': 'spanisch-deutsch'
+            }
+        };
+        return '//dict.leo.org/' +
+            translationUrls[srcLng][dstLng] +
+            '/' + word;
+    }
+}
 ];
 
 function appendIFrames() {
     services.forEach(function(service) {
-        $('#iframes').append('<iframe id="'+service.name+'Frame" src="about:blank" width="600" height="400"><p>Whoopsie! This prototype requires IFRAMEs to work. Sorry!</p></iframe>');
+        $('#iframes').append('<iframe id="' + service.name + 'Frame" src="about:blank" width="600" height="400"><p>Whoopsie! This prototype requires IFRAMEs to work. Sorry!</p></iframe>');
     });
 }
 
@@ -47,7 +46,7 @@ function assembleUrls() {
         const dstLng = $('#dstLng').val();
         const word = encodeURIComponent($('#word').val());
         const targetUrl = service.generateLookupUrl(srcLng, dstLng, word);
-        $('#'+service.name+'Frame').attr('src', targetUrl);
+        $('#' + service.name + 'Frame').attr('src', targetUrl);
     });
 }
 
