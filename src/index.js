56f913821602f061a5896a17d34f16952f24c896
Martin Unzner <martin.u@posteo.de>
2020-02-03 11:36:15 +0100

    import old web client layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Results extends React.Component {
+    render() {
+        return (
+            <div>
+            {/* TODO */}
+            </div>
+        );
+    }
+}
+
+class QueryForm extends React.Component {
+    render() {
+        return (
+            <form>
+            <p>What do you want to look up?</p>
+            <p><input type="text" id="word" /></p>
+            <p>
+            <select id="srcLng" size="1">
+            <option>Deutsch</option>
+            </select>
+            ↔
+            <select id="dstLng" size="1">
+            <option>English</option>
+            <option>Français</option>
+            <option>Italiano</option>
+            <option>Español (Si!)</option>
+            </select>
+            </p>
+            <p><button type="button" id="search">Search</button></p>
+            </form>
+        );
+    }
+}
+
+class Client extends React.Component {
+    render() {
+        return (
+            <div>
+            <QueryForm />
+            <Results />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Client />,
+    document.getElementById('root')
+);
